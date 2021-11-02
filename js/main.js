7baff0b2490149381ebe2e808f6219cc2a395553
Elena <myone72@yahoo.com>
2021-11-01 21:58:49 -0400

    create js for changing header's background color and deleting merging-top

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-$(document).ready((function(){$(".header__burger").click((function(e){$(".header__burger, .header__menu").toggleClass("active"),$("body").toggleClass("lock")}))}));
+$(document).ready(function(){
+	$('.header__burger').click(function(event){
+		$('.header__burger, .header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+$(function() {
+	$(window).on("scroll", function() {
+		 if($(window).scrollTop() > 800) {
+			  $(".header").addClass("changeBgc");
+			  $(".header").removeClass("marginHeader");
+		 } else {
+			  //remove the background property so it comes transparent again (defined in your css)
+			 $(".header").removeClass("changeBgc");
+			 $(".header").addClass("marginHeader");
+		 }
+	});
+});
